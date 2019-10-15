da4a88aee6b8262e2ce5914a62394a8cd58b7c13
baothi <baothi246@gmail.com>
2019-10-14 23:47:11 -0700

    demo python

--- polls/views.py
+++ polls/views.py
@@ -19,3 +19,14 @@ def viewlist(request):
 def detailView(request, question_id):
     q = Question.objects.get(pk=question_id)
     return render(request,"polls/detail_question.html", {"qs": q})
+
+def vote(request, question_id):
+	q = Question.objects.get(pk=question_id)
+	try:
+	    dulieu = request.POST["choice"]
+	    c = q.choice_set.get(pk = dulieu)
+	except:
+		HttpResponse("loi khong co choice")
+	c.vote = c.vote + 1
+	c.save()
+	return render(request, "polls/result.html", {"q":q})
